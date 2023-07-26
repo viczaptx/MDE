14141ae2249b8df9179c818c233102fa21320e0d
viczaptx <vzapata99@hotmail.com>
2023-07-26 16:36:58 -0500

    Add files via upload

---
+++ SAMPLE - MDE_Vulns_by_Device_to_SQL.ps1
@@ -0,0 +1,90 @@
+﻿# SQL SERVER INFO
+$Server = "<SQL SERVER NAME or IP>"
+$Db = "<DATABASE NAME - SHORTNAME>"      ### EXAMPLE: "VulnsDb"
+$Table = "<FULL TABLE PATH>"             ### EXAMPLE: "VulnsDb.dbo.Findings"
+$tempFile = "<PATH TO CSV OUTPUT FILE>"  ### EXAMPLE: "c:\Temp\DeviceVulns.csv"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# That code gets the App Context Token
+# Paste below your Tenant ID, App ID and App Secret (App key).
+
+$tenantId =  ### Paste your tenant ID here
+$appId =     ### Paste your Application ID here
+$appSecret = ### Paste your Application secret here
+
+$resourceAppIdUri = 'https://api.securitycenter.microsoft.com'
+$oAuthUri = "https://login.microsoftonline.com/$TenantId/oauth2/token"
+$authBody = [Ordered] @{
+     resource = "$resourceAppIdUri"
+     client_id = "$appId"
+     client_secret = "$appSecret"
+     grant_type = 'client_credentials'
+}
+
+$authResponse = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $authBody -ErrorAction Stop
+$token = $authResponse.access_token
+    
+#NOTE: Build our headers to ensure we can access the information.
+$headers = @{ 
+    'Content-Type' = 'application/json'
+    Accept         = 'application/json'
+    Authorization  = "Bearer $token" 
+}
+
+# GET VULNERABILITY DATA PER ENDPOINT
+$vulnerabilityUrl = "https://api.securitycenter.windows.com/api/vulnerabilities/machinesVulnerabilities?"
+$vulnerabilityData = (Invoke-WebRequest -Method Get -Uri $vulnerabilityUrl -Headers $headers -ErrorAction Stop).Content | ConvertFrom-Json
+
+$machineURL = "https://api.securitycenter.windows.com/api/machines"
+$machineData = (Invoke-WebRequest -Method Get -Uri $machineURL -Headers $headers -ErrorAction Stop).Content | ConvertFrom-Json
+    
+$endingData = New-Object -TypeName System.Collections.Generic.List[PsObject]
+foreach ($machine in $machineData.value) {
+    $MachineVulnerabilitylist = $vulnerabilityData.value | Where-object { $_.MachineID -eq $machine.ID }
+    foreach ($vulnerability in $MachineVulnerabilitylist) {
+        $machineDataHash = [ordered]@{
+            MachineName           = $machine.computerDnsName
+            MachineID             = $machine.id
+            lastSeen              = $machine.lastSeen
+            OSPlatform            = $machine.osPlatform
+            lastIpAddress         = $machine.lastIpAddress
+            CVE                   = $vulnerability.cveID
+            productName           = $vulnerability.productName
+            productVendor         = $vulnerability.productVendor
+            fixingKBID            = $(if ($null -eq $vulnerability.fixingKBID) { $vulnerability.fixingKbId }; if ($null -ne $vulnerability.fixingKBID) { "KB$($vulnerability.FixingKBID)" })
+        }
+        $vuln = New-Object -typename PSobject -property $machineDataHash
+        $endingData.Add($vuln) | Out-Null
+    }
+}
+
+# WRITE RESULTS TO CSV FILE (remove header row)
+$endingData | Export-Csv $tempFile -NoTypeInformation
+(Get-Content $tempFile | Select-Object -Skip 1) | Set-Content $tempFile
+
+# IMPORT CSV TO SQL
+$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$SqlConnection.ConnectionString = "Server=$Server;Database=$Db;Integrated Security=True;"
+$SqlConnection.Open()
+
+$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+$SqlCmd.Connection = $SqlConnection
+
+$Command = @"
+    BULK INSERT $Table
+    FROM '$tempFile'
+    WITH
+    (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+    )
+"@
+
+$SqlCmd.CommandText = $Command
+$SqlCmd.ExecuteNonQuery()
+$SqlConnection.Close()
+
+# CLEANUP TEMP FILE
+Remove-Item -Path $tempFile -Force
